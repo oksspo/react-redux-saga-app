d74046c349833ab4f835e2d3c0b319d4dfc669c8
Oksana Spolnyk <ospolnyk@brightgrove.com>
2020-10-24 18:13:24 +0300

    Adjusted basic webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    }
+	entry: './src/index.js',
+	output: {
+		path: `${path.resolve(__dirname)}/build`,
+		publicPath: '/',
+		filename: 'main.js'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js?$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env']
+					}
+				}
+			},
+			{
+				test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				enforce: 'pre',
+				loader: 'eslint-loader'
+			}
+		]
+	},
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.join(__dirname, './public/index.html'),
+			filename: 'index.html',
+			inject: 'body'
+		})
+	],
+
+	devtool: 'source-map',
+
+	devServer: {
+		host: 'localhost',
+		port: 8081,
+		contentBase: `${path.resolve(__dirname)}/build`,
+		hot: true
+	}
 };
