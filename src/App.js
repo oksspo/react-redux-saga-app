10a9f2a0a36c918f17cf6fe68518e8bf0cc68137
Oksana Spolnyk <ospolnyk@brightgrove.com>
2020-10-24 18:53:43 +0300

    Added dummy stepper from material UI

--- src/App.js
+++ src/App.js
@@ -1,21 +1,102 @@
+import {makeStyles} from '@material-ui/core/styles';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 import './App.css';
+import AppBar from "@material-ui/core/AppBar";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%'
+	},
+	button: {
+		marginRight: theme.spacing(1)
+	},
+	instructions: {
+		marginTop: theme.spacing(1),
+		marginBottom: theme.spacing(1)
+	}
+}));
+
+function getSteps() {
+	return ['Sign in', 'Write a post', 'Write a comment'];
+}
+
+function getStepContent(step) {
+	switch (step) {
+		case 0:
+			return 'Select campaign settings...';
+		case 1:
+			return 'What is an ad group anyways?';
+		case 2:
+			return 'This is the bit I really care about!';
+		default:
+			return 'Unknown step';
+	}
+}
 
 function App() {
+	const classes = useStyles();
+	const [activeStep, setActiveStep] = React.useState(0);
+	const [skipped, setSkipped] = React.useState(new Set());
+	const steps = getSteps();
+
+	const handleNext = () => {
+		let newSkipped = skipped;
+		setActiveStep((prevActiveStep) => prevActiveStep + 1);
+		setSkipped(newSkipped);
+	};
+
+	const handleReset = () => {
+		setActiveStep(0);
+	};
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<AppBar position="static" elevation={0}>
+				<Stepper activeStep={activeStep}>
+					{steps.map((label) => {
+						const stepProps = {};
+						const labelProps = {};
+
+						return (
+							<Step key={label} {...stepProps}>
+								<StepLabel {...labelProps}>{label}</StepLabel>
+							</Step>
+						);
+					})}
+				</Stepper>
+			</AppBar>
+
+			<div>
+				{activeStep === steps.length ? (
+					<div>
+						<Typography className={classes.instructions}>
+							All steps completed - you&apos;re finished
+						</Typography>
+						<Button onClick={handleReset} className={classes.button}>
+							Reset
+						</Button>
+					</div>
+				) : (
+					<div>
+						<Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+						<div>
+							<Button
+								variant="contained"
+								color="primary"
+								onClick={handleNext}
+								className={classes.button}
+							>
+								{activeStep === steps.length - 1 ? 'Finish' : 'Next'}
+							</Button>
+						</div>
+					</div>
+				)}
+			</div>
 		</div>
 	);
 }
