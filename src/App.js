a27403eaf9cd0130065ed1292de2bf8542bda888
Oksana Spolnyk <ospolnyk@brightgrove.com>
2020-10-25 23:23:40 +0200

    Changed stepper for small screens

--- src/App.js
+++ src/App.js
@@ -9,26 +9,34 @@ import {getSteps, getActiveStep} from './utils';
 import './App.css';
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
+import StepContent from "@material-ui/core/StepContent";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 
 function App(props) {
 	const steps = getSteps();
+	const matches = useMediaQuery('(max-width:425px)');
+
 	return (
 		<Box component="main" className="App">
 			<AppBar position="static" elevation={0}>
-				<Stepper activeStep={props.activeStep}>
+				<Stepper activeStep={props.activeStep} orientation={matches ? 'vertical' : 'horizontal'}>
 					{steps.map((label) => {
 						return (
 							<Step key={label}>
 								<StepLabel>{label}</StepLabel>
+
+								{matches && (<StepContent>
+									{props.children}
+								</StepContent>)}
 							</Step>
 						);
 					})}
 				</Stepper>
 			</AppBar>
 
-			<Container maxWidth="sm">
+			{!matches && <Container maxWidth="sm">
 				{props.children}
-			</Container>
+			</Container>}
 
 			<Confirmation/>
 		</Box>
